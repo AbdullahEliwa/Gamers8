60c6041bd17b432eb9b55857bb3eb342c1c30086
AshrafNosair <anosair@meemdev.com>
2022-05-10 15:15:11 +0200

    Attractio class

---
+++ Gamers8.Core/Entities/AttractionsAggregate/Attraction.cs
@@ -0,0 +1,30 @@
+﻿using Booking.Core.ValueObjects;
+using Gamers8.Core.Entities.Base;
+using Gamers8.Core.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamers8.Core.Entities.AttractionsAggregate
+{
+    public class Attraction : AuditableEntity<Guid>
+    {
+        public string Title { get;  set; }
+        public AttractionCategories AttractionCategory { get;  set; }
+        public string DefaultImagePath { get;  set; }
+        public string Description { get;  set; }
+        public SeasonZones SeasonZone { get;  set; }
+        public DateTimeRange DateTime { get; set; }
+        public string OpeningTimes { get; set; }
+        public int AgeGroupFrom { get; set; }
+        public bool IncludedInEntranceTicket { get; set; } = false;
+        public bool RequiresSignup { get; set; } = false;
+        public bool IsFeaturedEvent { get; set; } = false;
+
+
+
+
+    }
+}
