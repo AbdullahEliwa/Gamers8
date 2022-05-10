4c4c7477c0f2cd0c85c18cecd9d444ae7e46b02b
abdullah eliwa <aeliwah@meemdev.com>
2022-05-10 16:44:09 +0200

    Summit Day - Session.

---
+++ Gamers8.Core/Entities/SummitAggregate/SummitDay.cs
@@ -0,0 +1,14 @@
+﻿using Gamers8.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamers8.Core.Entities.SummitAggregate
+{
+    public class SummitDay : AuditableEntity<int>
+    {
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Gamers8.Core/Entities/SummitAggregate/SummitSession.cs
@@ -0,0 +1,21 @@
+﻿using Booking.Core.ValueObjects;
+using Gamers8.Core.Entities.Base;
+using Gamers8.Core.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamers8.Core.Entities.SummitAggregate
+{
+    public class SummitSession : AuditableEntity<Guid>
+    {
+        public DescriptionLocalized Title { get; set; }
+        public DateTimeRange DateTime { get; set; }
+
+        public DescriptionLocalized Desciption { get; set; }
+        public SeasonZones SeasonZone { get; set; }
+
+    }
+}
